7561ad61c0ca1e53c22a891c16328bd1f6134b9c
Filbertik <aton2019zvit@gmail.com>
2025-04-28 00:03:21 +0300

    variant 5

--- src/redux/store.js
+++ src/redux/store.js
@@ -7,3 +7,19 @@ export const store = configureStore({
 });
 
 export default store;
+
+// test acces to  api
+
+// fetch("https://680e843e67c5abddd19257bf.mockapi.io")
+//   .then((response) => {
+//     if (!response.ok) {
+//       throw new Error("Network response was not ok");
+//     }
+//     return response.json();
+//   })
+//   .then((data) => {
+//     console.log("Data:", data);
+//   })
+//   .catch((error) => {
+//     console.error("There was a problem with the fetch operation:", error);
+//   });
